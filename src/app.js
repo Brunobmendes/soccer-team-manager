2fdb7a1479ad82789ebe6e8cd37ffc6cdddcc535
Brunobmendes <Bruno.mendes2804@gmail.com>
2023-01-31 16:17:14 -0300

    create a api for soccer teams

--- src/app.js
+++ src/app.js
@@ -1,5 +1,54 @@
 const express = require('express');
 
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+];
+
 const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+app.get('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const findTeam = teams.find((team) => team.id === Number(id));
+
+  if (!findTeam) {
+    res.status(404).json({ message: 'team not found' });
+  }
+
+  res.status(202).json({ team: findTeam });
+});
+
+app.post('/teams', (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
+
+app.put('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, initials } = req.body;
+
+  const updateTeam = teams.find((team) => team.id === Number(id));
+
+  if (!updateTeam) {
+    res.status(404).json({ message: 'Team not found' });
+  }
+
+  updateTeam.name = name;
+  updateTeam.initials = initials;
+  res.status(200).json({ updateTeam });
+});
 
 module.exports = app; 
